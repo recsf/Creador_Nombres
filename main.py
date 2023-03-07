9bad7d9281b63aae17f728f362d33b7123e499ef
recsf <erickgarciagsf@gmail.com>
2023-03-06 23:50:16 -0600

    Día uno, proyecto 1

---
+++ main.py
@@ -0,0 +1,5 @@
+# colocamos los inputs
+etiqueta1 = input("Nombre de tu equipo favorito: ")
+etiqueta2 = input("Tu numero favorito: ")
+# imprimimos resultado
+print('El nombre de la cerveza es: \n"' + etiqueta1 + " " + etiqueta2 + '"')
